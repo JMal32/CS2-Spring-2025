2016a01a7bb1dc8c76eb29ddc9189e855e7cbc99
Justin Malone <jmalone@Mac.home.arpa>
2025-02-23 23:43:05 -0600

    working on charstack and intstack. will start building palindrome now.

---
+++ lab3/stacks.cpp
@@ -0,0 +1,13 @@
+#include "intStack.h"
+#include "charStack.h"
+#include <iostream>
+using namespace std;
+
+// function to check if the given string is a palindrome
+int main() {
+  string str;
+  cout << "Enter a string: ";
+  cin >> str;
+  int len = str.length();
+
+}
