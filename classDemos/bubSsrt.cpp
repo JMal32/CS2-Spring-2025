c31659e345ffacababeff45f1c0dbe7d5949a89b
Justin Malone <jmalone@wifi-bsustu-10-3-014-048.bemidjistate.edu>
2025-04-07 12:16:28 -0500

    started messing with bubblesort

---
+++ classDemos/bubSsrt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+void bubbleSort(int list) {
+  for (int i = 0; i < list - 1; i++) {
+   for (int x = 0; i < list - 1; i++){
+      // if alist i > alist i+1 then swap ai 
+    } 
+  }
+}
--- lab4b/radLink.h
+++ lab4b/radLink.h
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <stdexcept>
 
-template <typename T> class radLink {
-public:
-  radLink(); // Constructor
-  ~radLink(); // Destructor
+template <typename T> class LinkedList2 {
+
 private:
   /* After doing some reearch, I am going to put my node in this class file
   as a struct.*/
@@ -16,5 +14,15 @@ private:
   Node* head; // Pointer to first node
   Node* tail; // Pointer to last node
   int count; // This is where I track the nodes
+
+public:
+  LinkedList2() : head(nullptr), tail(nullptr), count(0) {} // Constructor
+  ~LinkedList2() {  // Destructor
+    while (head != nullptr) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
 };
 
